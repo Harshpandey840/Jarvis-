1945ffd4650f315c9bd446be22b48d121e81a35d
Harshpandey840 <info2harsh84@gmail.com>
2026-09-12 20:00:54 +0530

    Update MainActivity.kt

--- app/src/main/java/com/user/jarvis/MainActivity.kt
+++ app/src/main/java/com/user/jarvis/MainActivity.kt
@@ -80,6 +80,8 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
             putExtra(RecognizerIntent.EXTRA_LANGUAGE, "hi-IN")
             putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, false)
+            putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 2500L)
+            putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, 2500L)
         }
         statusText.text = "Sun raha hoon..."
         speechRecognizer.startListening(intent)
