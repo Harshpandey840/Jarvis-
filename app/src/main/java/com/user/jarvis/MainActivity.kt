0fcba5916a0606033e2ae0e060b815949878866b
Harshpandey840 <info2harsh84@gmail.com>
2026-09-12 20:53:21 +0530

    Update MainActivity.kt

--- app/src/main/java/com/user/jarvis/MainActivity.kt
+++ app/src/main/java/com/user/jarvis/MainActivity.kt
@@ -78,7 +78,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         }
         val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
             putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-            putExtra(RecognizerIntent.EXTRA_LANGUAGE, "hi-IN")
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-IN")
             putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, false)
             putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 2500L)
             putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, 2500L)
